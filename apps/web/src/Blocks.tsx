b390aed86da69484c0af3206f6a129a35ff3b726
alexy-os <agorbunoff.ru@gmail.com>
2025-07-21 23:54:09 +0300

    Refactor Blocks and components for a cleaner codebase. Still in the process of setting up parsers to clear blocks. So not all blocks have correct layout

--- apps/web/src/Blocks.tsx
+++ apps/web/src/Blocks.tsx
@@ -1,17 +1,19 @@
-// import HeroBlocks from "./blocks/hero";
+import HeroBlocks from "./blocks/hero";
 // import BlogBlocks from "./blocks/blog";
 // import BusinessBlocks from "./blocks/business";
 // import FeaturesBlocks from "./blocks/features";
 import CTABlocks from "./blocks/cta";
+import { FooterFourColumns } from "@twblocks/blocks/footer/FooterFourColumns";
 
 function Blocks() {
   return (
     <div>
-      {/*<HeroBlocks />*/}
+      <HeroBlocks />
       {/*<BlogBlocks />*/}
       {/*<BusinessBlocks />*/}
       {/*<FeaturesBlocks />*/}
       <CTABlocks />
+      <FooterFourColumns />
     </div>
   );
 }
