b29c7778dd026a9371a98414a004dde2570805fd
alexy-os <agorbunoff.ru@gmail.com>
2025-09-01 14:16:47 +0300

    brain hub removed - brainstorming from chat in cursor ide

--- packages/@ui8kit/brain/.draft/.old/scripts-ts/generateComponentsQDrant.ts
+++
@@ -1,8 +0,0 @@
-import { QdrantList } from './utils/qdrantList';
-
-async function main() {
-  const qdrant = new QdrantList();
-  await qdrant.generateQdrantList();
-}
-
-main().catch(console.error);
